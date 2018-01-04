0faaa95fd0073b269010220639f4319af0146903
ravisharmaa <bastola.ravi@gmail.com>
2018-01-04 17:37:45 +0545

    Components Testing

---
+++ resources/assets/js/components/DashboardComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="hero is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                This is Dashboard
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        data () {
+          return {
+
+          }
+        },
+        mounted() {
+            console.log('Dashboard Component mounted.')
+        }
+    }
+</script>
--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,48 +1,21 @@
 <template>
-    <section class="hero is-fullheight">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Login</h3>
-                    <p class="subtitle has-text-grey">Please login to proceed.</p>
-                    <div class="box">
-                        <figure class="avatar">
-                            <img src="https://placehold.it/128x128">
-                        </figure>
-                        <form>
-                            <div class="field">
-                                <div class="control">
-                                    <input class="input is-success" type="text" placeholder="Text input" value="bulma">
-                                </div>
-                            </div>
-
-                            <div class="field">
-                                <div class="control">
-                                    <input class="input is-success" type="text" placeholder="Text input" value="bulma">
-                                </div>
-                            </div>
-                            <div class="field">
-                                <label class="checkbox">
-                                    <input type="checkbox">
-                                    Remember me
-                                </label>
-                            </div>
-                            <a class="button is-block is-info is-large">Login</a>
-                        </form>
-                    </div>
-                    <p class="has-text-grey">
-                        <a href="../">Sign Up</a> &nbsp;·&nbsp;
-                        <a href="../">Forgot Password</a> &nbsp;·&nbsp;
-                        <a href="../">Need Help?</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </section>
+    <login></login>
+    <dashboard v-if="authenticated"></dashboard>
 </template>
 
 <script>
+    import Login from '../components/LoginComponent'
+    import Dashboard from '../components/DashboardComponent'
     export default {
+        data () {
+          return {
+              authenticated:false
+          }
+        },
+        components:{
+          'Login':Login,
+          'Dashboard':Dashboard
+        },
         mounted() {
             console.log('Component mounted.')
         }
